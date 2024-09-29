9722b45a1e0c58158475c5abc1c18be614a18ed2
boring-dude <parvind97@gmail.com>
2024-09-29 16:30:40 +0530

    Tool: Add more kutty tools
    Check if a package exists before installing it

--- first-time/install-pkg.sh
+++ first-time/install-pkg.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+# This approach fails if package name differs between OSes
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     installer="sudo apt-get install"
+    # info="dpkg -s" # Untested
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     installer="brew install"
+    info="brew list -1"
 fi
 
+# This approach fails when package name differs from command name
+# Eg: derailed/k9s/k9s is installed via brew but the command is k9s
+function is_installed() {
+    $info | grep $1 > /dev/null
+    return $?
+}
+
 function install_pkg() {
-    eval "$installer $1"
+    is_installed $1 || eval "$installer $1"
 }
