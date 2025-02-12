3780f67d0f571895a0d5db7c66535b4b0cffc498
boring-dude <parvind97@gmail.com>
2025-02-12 10:13:28 +0530

    Setup: Use source instead of sh

--- first-time/init.sh
+++ first-time/init.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 SCRIPT_DIR=$DOTFILES/first-time
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  sh $SCRIPT_DIR/linux.sh  || exit 1
+  source $SCRIPT_DIR/linux.sh  || exit 1
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-  sh $SCRIPT_DIR/macos.sh  || exit 1
+  source $SCRIPT_DIR/macos.sh  || exit 1
 fi
 
-sh $SCRIPT_DIR/install-oh-my-zsh.sh
-sh $SCRIPT_DIR/install-powerlevel10k.sh
-sh $SCRIPT_DIR/install-mise.sh
+source "$SCRIPT_DIR/install-oh-my-zsh.sh"
+source "$SCRIPT_DIR/install-powerlevel10k.sh"
+source "$SCRIPT_DIR/install-mise.sh"
